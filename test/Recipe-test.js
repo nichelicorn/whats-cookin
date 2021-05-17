49014acff4cd66f1b72c866f8ae6f92460dfdc36
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-17 15:34:41 -0600

    ✨ Add test data and hook to Recipe-test

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,3 +1,66 @@
+import { expect } from 'chai';
+import Recipe from '../src/classes/Recipe';
+
+describe('Recipe', () => {
+  let recipe1, recipe2;
+
+  beforeEach(() => {
+    recipe1 = {
+      "id": 1,
+      "image": "https://soufflebombay.com/wp-content/uploads/2017/01/Fried-Egg-Avocado-Rice-Bowl.jpg",
+      "ingredients": [
+        {
+          "id": 0,
+          "quantity": {
+            "amount": 2,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 1,
+          "quantity": {
+            "amount": 1,
+            "unit": "large"
+          }
+        },
+        {
+          "id": 2,
+          "quantity": {
+            "amount": 1,
+            "unit": "large"
+          }
+        }
+      ],
+      "instructions": [
+        {
+          "instruction": "Cook rice.",
+          "number": 1
+        },
+        {
+          "instruction": "Fry egg.",
+          "number": 2
+        },
+        {
+          "instruction": "Slice avocado.",
+          "number": 3
+        },
+        {
+          "instruction": "Once rice is cooked, scoop out desired portion into a bowl and top with egg and avocado slices. Garnish with chives and lime wedge.",
+          "number": 4
+        }
+      ],
+      "name": "Rice bowl with Fried Egg",
+      "tags": [
+        "breakfast",
+        "morning meal",
+        "snack",
+        "appetizer"
+      ]
+    }
+  })
+
+})
+
 // 📝 Recipe class
 // a new `Recipe` represents one recipe object
 // it should store all the details provided in the data file
