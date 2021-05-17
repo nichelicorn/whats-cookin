81f0f430cb459166ab592e42d7d26c4cc8f804cb
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-17 14:02:40 -0600

    ✨ Add hook to Ingredient-test

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -2,6 +2,26 @@ import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
 
 describe('Ingredient', () => {
+  let ingr1, ingr2, ingr3;
+
+  beforeEach(() => {
+    ingr1 = {
+      "id": 1,
+      "name": "rice",
+      "estimatedCostInCents": 150
+    },
+    ingr2 = {
+      "id": 2,
+      "name": "egg",
+      "estimatedCostInCents": 10
+    },
+    ingr3 = {
+      "id": 3,
+      "name": "avocado",
+      "estimatedCostInCents": 250
+    }
+  });
+
   it.skip('should be a function', () => {
     expect(Ingredient).to.be.a('function');
   })
