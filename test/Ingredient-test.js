9fcd541cdfda619233f5e8cd9ebe975d78565c53
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-17 13:59:07 -0600

    ✨ Set up initial describe block for Ingredient

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -1,2 +1,8 @@
 import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
+
+describe('Ingredient', () => {
+  it.skip('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  })
+})
