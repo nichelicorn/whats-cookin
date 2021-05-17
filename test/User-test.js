5abaee7f9d30d3e18215ffaf1417e7536c19c49a
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-17 14:44:54 -0600

    Write two tests to address properties for User class

--- test/User-test.js
+++ test/User-test.js
@@ -1,8 +1,30 @@
 import { expect } from 'chai';
-import RecipeRepository from '../src/classes/RecipeRepository';
+import User from '../src/classes/User';
 
-describe('Recipe', () => {
-  it('Should be a function', () => {
-    expect(RecipeRepository).to.be.a('function');
+describe('User', function() {
+  let user;
+
+  beforeEach(() => {
+    user = new User()
+    user.favoriteRecipes = []
+    user.recipesToCook = []
   });
-})
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+    });
+
+    it('should instantiate a User', function() {
+      expect(user).to.be.an.instanceof(User);
+    });
+
+    it('should be able to store favorite recipes', function() {
+      expect(user.favoriteRecipes).to.be.an('array')
+      expect(user.favoriteRecipes).to.deep.equal([])
+    })
+
+    it('should be able to store recipes to cook', function() {
+      expect(user.recipesToCook).to.be.an('array')
+      expect(user.recipesToCook).to.deep.equal([])
+    })
+  });
