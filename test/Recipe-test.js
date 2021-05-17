e0cee3cec5a1ac162647d203d944f2ca574a9eb4
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-17 15:02:31 -0600

    📝 Add pseudocode for Recipe.constructor

---
+++ test/Recipe-test.js
@@ -0,0 +1,25 @@
+// 📝 Recipe class
+// a new `Recipe` represents one recipe object
+// it should store all the details provided in the data file
+// it should have a method to determine the names of ingredients needed to make a Recipe
+// it should have a method to determine the cost of ingredients for a Recipe
+// it should have a method to return ingredients / instructions for a Recipe
+
+// 📝 Recipe.constructor
+// it should store a property `id` whose value is a number
+
+// 📝 Recipe.constructor
+// it should store a property `image` whose value is a string representing a url/image source path
+
+// 📝 Recipe.constructor
+// it should store a property `ingredients` whose value is an array of objects
+// refactor to instantiate each ingredient in this array as a `new Ingredient()` instantiation
+
+// 📝 Recipe.constructor
+// it should store a property `instructions` whose value is an array of objects
+
+// 📝 Recipe.constructor
+// it should a property `name` whose value is a string
+
+// 📝 Recipe.constructor
+// it should store a property `tags` whose value is an array of strings
