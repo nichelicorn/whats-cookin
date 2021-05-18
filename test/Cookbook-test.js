52727da94c2efcfcf92225e57226bf9492290ab8
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-18 14:32:22 -0600

    📝 Add pseudocode

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -6,3 +6,16 @@ describe('Recipe Repository', () => {
     expect(Cookbook).to.be.a('function');
   });
 })
+
+// 📔 Cookbook
+// it should store all recipe objects
+
+// 📔 Cookbook.constructor
+// it should take in recipe objects and add them to an array
+// it should be able to take in a single recipe object and add it to the existing recipes
+// ex. this.cookbook = [{recipeData}] || {recipe}
+
+// 📔 Cookbook.filter
+// it should have a method to filter through tags and return recipe objects containing requested tags
+// it should have a method to filter through recipe names and return matching results
+// it should have a method to filter through ingredients and return recipes containing those ingredients
