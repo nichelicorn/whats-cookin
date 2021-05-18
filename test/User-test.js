e8e7940f60bdcd11eb120aabbe0bbe8e2b652c8e
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-17 19:38:24 -0600

    Include final test for User file to filter through recipes

--- test/User-test.js
+++ test/User-test.js
@@ -6,9 +6,11 @@ import User from '../src/classes/User';
 describe('User', function() {
   let user1;
   let recipe1;
+  let ingr1;
 
   beforeEach(() => {
     user1 = new User()
+    ingr1 = new Ingredient ( 0, 'rice', 150 );
     user1.favoriteRecipes = []
     user1.recipesToCook = []
     recipe1 = new Recipe(1, "https://soufflebombay.com/wp-content/uploads/2017/01/Fried-Egg-Avocado-Rice-Bowl.jpg", [{
@@ -96,8 +98,21 @@ describe('User', function() {
     expect(user1.recipesToCook).to.deep.equal([recipe1])
   });
 
-  it.skip('should filter through recipes by name, tag, or ingredients', function() {
-    user1.filterRecipes(tag);
-    expect(user.filterRecipes(tag)).to.equal(-- -- --)
+  it.skip('should filter through recipes by tag or name', function() {
+    user1.filterRecipes("avocado");
+    expect(user.filterRecipes("avocado")).to.equal(recipe1);
+    user1.filterRecipes("Rice bowl with Fried Egg");
+    expect(user.filterRecipes("Rice bowl with Fried Egg")).to.equal(recipe1);
   });
+
+  it.skip('should instantiate an Ingredient', function() {
+    expect(ingr1).to.be.an.instanceof(Ingredient);
+  });
+
+  it.skip('should filter through recipes by ingredient', function() {
+    user1.filterRecipes("rice");
+    expect(user.filterRecipes("rice")).to.equal(ingr1)
+  });
+
+
 });
