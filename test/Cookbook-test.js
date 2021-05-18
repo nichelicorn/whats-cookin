18c58cbfcefa864498fb5d204cb878d1d146a4f7
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-18 16:31:38 -0600

    ✨ Add test for Cookbook.filterByIngredient

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -68,8 +68,13 @@ describe('Recipe Repository', () => {
 
     // it should have a method to filter through ingredients and return recipes containing those ingredients
     it.skip('should be able to search recipes by ingredient', () => {
+      let ingrSearch = testCookbook.filterByIngredient('TOMATILLO');
+      expect(nameSearch).to.deep.equal(recipe2);
+    })
+
+    it.skip('should return all recipes containing a particular ingredient', () => {
       let ingrSearch = testCookbook.filterByIngredient('avocado');
-      expect(nameSearch).to.deep.equal([])
+      expect(nameSearch).to.deep.equal([recipe1, recipe2])
     })
   })
 })
