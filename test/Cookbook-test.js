93d2070aea3ea4dc727d8eb17f4bb43158daff6f
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-18 14:06:29 -0600

    ♻️ Rename RecipeRepository to Cookbook

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
-import RecipeRepository from '../src/classes/RecipeRepository';
+import Cookbook from '../src/classes/Cookbook';
 
 describe('Recipe Repository', () => {
   it('Should be a function', () => {
-    expect(RecipeRepository).to.be.a('function');
+    expect(Cookbook).to.be.a('function');
   });
 })
