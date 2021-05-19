3cbe7e06ac94c68294812cb3da8d30bed2ccdf60
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-19 15:41:19 -0600

    ♻️  Refactor test data - don't have Recipe class yet to build objects

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -1,12 +1,66 @@
 import { expect } from 'chai';
 import { testRecipes, testRecipeData } from '../test/test-data';
 import Cookbook from '../src/classes/Cookbook';
+import Recipe from '../src/classes/Recipe';
 
 describe('Cookbook', () => {
   let recipe1, recipe2, recipe3, testData, testCookbook;
 
   beforeEach(() => {
-    recipe1 = testRecipes[0];
+    // recipe1 = testRecipes[0]; // this isn't working (wrong setup)
+    // recipe1 = new Recipe(testRecipes[0]); // this isn't working because there isn't a Recipe class yet to construct the new Recipe object
+    recipe1 = {
+      "id": 1,
+      "image": "https://soufflebombay.com/wp-content/uploads/2017/01/Fried-Egg-Avocado-Rice-Bowl.jpg",
+      "ingredients": [
+        {
+          "id": 0,
+          "quantity": {
+            "amount": 2,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 1,
+          "quantity": {
+            "amount": 1,
+            "unit": "large"
+          }
+        },
+        {
+          "id": 2,
+          "quantity": {
+            "amount": 1,
+            "unit": "large"
+          }
+        }
+      ],
+      "instructions": [
+        {
+          "instruction": "Cook rice.",
+          "number": 1
+        },
+        {
+          "instruction": "Fry egg.",
+          "number": 2
+        },
+        {
+          "instruction": "Slice avocado.",
+          "number": 3
+        },
+        {
+          "instruction": "Once rice is cooked, scoop out desired portion into a bowl and top with egg and avocado slices. Garnish with chives and lime wedge.",
+          "number": 4
+        }
+      ],
+      "name": "Rice bowl with Fried Egg",
+      "tags": [
+        "breakfast",
+        "morning meal",
+        "snack",
+        "appetizer"
+      ]
+    };
     recipe2 = testRecipes[1];
     recipe3 = testRecipes[2];
     testData = testRecipeData;
@@ -37,6 +91,7 @@ describe('Cookbook', () => {
     // it should have a method to filter through tags and return recipe objects containing requested tags
     it.only('should filter by tags and return an array of recipes', () => {
       let tagSearch = testCookbook.filterByTag('breakfast');
+      console.log(tagSearch())
       expect(tagSearch[0].name).to.equal('Rice bowl with Fried Egg');
     })
 
