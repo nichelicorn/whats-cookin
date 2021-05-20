b6076af14c89fa39d4d103a3cfec40715f9d0921
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-19 19:46:14 -0600

    ♻️  Delete console.logs and pseudo

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -4,20 +4,11 @@ class Cookbook {
   }
 
   filterByTag(tagArr) {
-    // console.log('recipes <>>>', this.recipeData);
-    console.log('tagArr <>>>', tagArr); // only accepting 'breakfast'
     const tagFilterResults = this.recipeData.filter(recipe => {
       return tagArr.some(tag => {
         return recipe.tags.includes(tag);
       })
-
-      // return recipe.tags.includes(tagArr);
-      // if (recipe.tags.includes(tagArr)) {
-      //   console.log('recipe <>>>', recipe);
-      //   return recipe;
-      // }
     })
-    console.log('tagFilterResults <>>>', tagFilterResults);
     return tagFilterResults;
   }
 }
