cb96ecc83554d27c15c2953fbc3bab978026acaa
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-19 20:34:19 -0600

    ✨ Pass test for Cookbook.filterByName

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -12,8 +12,17 @@ class Cookbook {
     return tagFilterResults;
   }
 
-  filterByName(nameArr) {
-    console.log('hi')
+  filterByName(filtName) {
+    // console.log('filtName <>>>', filtName); // this is an array with a single string
+    const nameFilterResults = this.recipeData.filter(recipe => {
+      console.log('recipe.name <>>>', recipe.name); // this is a string
+      if (recipe.name.includes(filtName)) {
+        console.log('💩');
+        return recipe;
+      }
+      // return recipe.name.toLowerCase().includes(filtName);
+    })
+    return nameFilterResults;
   }
 }
 
