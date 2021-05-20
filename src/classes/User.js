f5c2c98d164432f2f2ba54df4b35f84f57084a38
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-19 21:34:37 -0600

    Add property for user name

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,8 +1,17 @@
 class User {
-  constructor() {
+  constructor(user) {
+    this.name = user.name
 
 
   }
 }
 
 export default User;
+
+
+// Create classes and methods that can:
+//
+// Allow a user to favorite or unfavorite recipes (add to / remove from the user’s favoriteRecipes)
+// Decide to cook a recipe that week (add to my recipesToCook)
+// Filter my favoriteRecipes by one or more tags.
+// Filter my favoriteRecipes by its name or ingredients.
