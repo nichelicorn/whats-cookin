9f36ab1c5b589838dee5173aed5387a52d0674a1
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-19 19:57:33 -0600

    ✅ Return to a state of green check mark for Cookbook.filterByTag

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -4,15 +4,10 @@ class Cookbook {
   }
 
   filterByTag(tagArr) {
-    const tagFilterResults = this.recipeData.filter(recipe => { // filter through the recipe data, and for each recipe
+    const tagFilterResults = this.recipeData.filter(recipe => {
       return tagArr.some(tag => {
-        // if (!recipe.tags.includes(tag)) {
-          // return [];
-        // }
-        // return a bool for the result of a comparison with the tagArr
-        return recipe.tags.includes(tag); // return the recipe if the tag is found in the recipe.tags
+        return recipe.tags.includes(tag);
       })
-      // need to add a conditional to return an empty array if a matching tag is not found in the array
     })
     return tagFilterResults;
   }
