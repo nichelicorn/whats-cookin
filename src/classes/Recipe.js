2bc21d0c2bfdc820b3c3b8fb59cc4520094b57df
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-19 20:25:02 -0600

    Begin calcCost method

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,18 +1,59 @@
-import { ingredientsData } from '../data/ingredients';
+import {
+  ingredientsData
+} from '../data/ingredients';
 import Ingredient from '../classes/Ingredient.js';
 
 class Recipe {
-  constructor(id, image, ingredients, instructions, name, tags) {
-    this.id = id
-    this.image = image
-    this.ingredients = [] // will bring in JSON data
-    this.instructions = instructions
-    this.name = name
-    this.tags = tags
-    this.cost = 0
+  constructor(recipe, ingredientsData) {
+    this.id = recipe.id
+    this.image = recipe.image
+    this.recipeIngredients = recipe.ingredients
+    this.instructions = recipe.instructions
+    this.name = recipe.name
+    this.tags = recipe.tags
+    this.ingredientNames = []
+    this.ingredientsData = ingredientsData
 
   }
+
+  returnInstructions() {
+    return this.instructions;
+  }
+
+  returnIngredients() {
+    console.log(this.recipeIngredients)
+    return this.recipeIngredients
+  }
+
+  returnIngredientNames() {
+  let getIngredients = this.recipeIngredients.reduce((arr, ingredient) => {
+      let foundIng = this.ingredientsData.find(ing => (ingredient.id === ing.id))
+      arr.push(foundIng)
+      return arr
+    }, []).forEach(ingred => {
+      return this.ingredientNames.push(ingred.name)
+    });
+    return this.ingredientNames
+
 }
+
+calculateCost() {
+  console.log(this.ingredientNames)
+
+
+}
+
+// It should have methods to:
+// Determine the names of ingredients needed
+// Get the cost of its ingredients
+// Return its directions / instructions
+
+}
+
+
+
+
+
 
 
 export default Recipe;
