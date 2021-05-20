f1c54b954d90a1a84e1d47faf5f51a7d0222c960
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-19 21:51:55 -0600

    Add one user for data in test

--- test/User-test.js
+++ test/User-test.js
@@ -2,15 +2,38 @@ import {
   expect
 } from 'chai';
 import User from '../src/classes/User';
+import Recipe from '../src/classes/Recipe';
 
 describe('User', function() {
   let user1;
   let recipe1;
   let ingr1;
+  let userData;
 
   beforeEach(() => {
+    userData = [
+      {
+        "id": 1,
+        "name": "Saige O'Kon",
+        "pantry": [
+          {
+            "ingredient": 11297,
+            "amount": 4
+          },
+          {
+            "ingredient": 1082047,
+            "amount": 10
+          },
+          {
+            "ingredient": 20081,
+            "amount": 5
+          },
+        ]
+      },
+    ]
+
     user1 = new User()
-    ingr1 = new Ingredient(0, 'rice', 150);
+
     recipe1 = new Recipe(1, "https://soufflebombay.com/wp-content/uploads/2017/01/Fried-Egg-Avocado-Rice-Bowl.jpg", [{
           "id": 0,
           "quantity": {
@@ -61,7 +84,7 @@ describe('User', function() {
       ]);
   })
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(User).to.be.a('function');
   });
 
