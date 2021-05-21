3e198bc61849a99e51bf2ea50574976dad2a9edb
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-21 12:27:32 -0600

    Begin with few query selectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,28 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+// console.log('Hello world');
+//---------------------Query Selectors---------------------//
+let myFavoritesBtn = document.getElementById('myFavoritesBtn');
+let recipesToCookBtn = document.getElementById('recipesCookLtrBtn');
+let mainPageContainer = document.getElementById('mainPageContainer');
+let category1 = document.getElementById('categoryTitle1');
+let category2 = document.getElementById('categoryTitle2');
+let category3 = document.getElementById('categoryTitle3');
+
+
+//----------------Global Variables -------------------------//
+// let recipe = new Recipe ();
+// let
+
+//----------------Event Listeners -------------------------//
+myFavoritesBtn.addEventListener('click', function() {
+  location.reload()
+});
+
+window.onclick = function() {
+  console.log("test", event.target.id)
+}
 
 
 
-
-console.log('Hello world');
+// -------------------Event Handlers -----------------------//
